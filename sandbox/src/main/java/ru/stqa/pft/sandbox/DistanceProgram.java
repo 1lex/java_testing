d88cabe5517acf577b0955bf3a14d672d4598195
1lex <suuurki@gmail.com>
2016-11-29 23:14:25 +0300

    В класс DistanceProgram добавлена функция distance
    Добавлена функция вычисления расстояния между двумя точками (пункт 2. из
    ДЗ)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceProgram.java
@@ -8,5 +8,10 @@ public class DistanceProgram {
       Point p1 = new Point(5,7);
       Point p2 = new Point(6,6);
       System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + "), вычисленное с помощью метода равно " + p1.distance(p2));
+      System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + "), вычисленное с помощью функции равно " + distance(p1, p2));
+   }
+
+   public static double distance (Point p1, Point p2) {
+      return Math.sqrt((p2.x * p2.x - 2 * p1.x * p2.x + p1.x * p1.x) + (p2.y * p2.y - 2 * p1.y * p2.y + p1.y * p1.y));
    }
 }
